0023b3bdf2d550fadfa477b6cde2e106d9c73a96
yatsuna827 <yatsuna_827@yahoo.co.jp>
2021-12-05 22:05:11 +0900

    BDSPバトルタワーのポケモン機能を追加

---
+++ src/pages/Gen8/BDSP/BattleTower/TrainerList.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const TrainerList: React.FC = () => {
+  return <>つかれた_(:3」∠)_</>
+}
---
+++ src/pages/Gen8/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Gen8Top: React.FC = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to="/PokemonRNGTools/Gen8/bdsp/battle-tower">バトルタワー</Link>
+        </li>
+      </ul>
+    </>
+  )
+}
